30bd388dd208489219b82f43f77a10cc1a3dbbc9
thousand0625 <88626463+NoranekoSan1000@users.noreply.github.com>
2022-07-09 11:16:43 +0900

    tes

--- Assets/Script/DoorMove.cs
+++ Assets/Script/DoorMove.cs
@@ -5,40 +5,11 @@ using UnityEngine;
 public class DoorMove : MonoBehaviour
 {
     bool IsMove = false;
-    float Rotate = 2f;
+    float Rotate=2f;
     float RoTimer = 0f;
     int Counter = 0;
 
-    /*HingeJoint hj;
-    JointLimits limits;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        hj = GetComponent<HingeJoint>();
-        limits = hj.limits;
-        IsMove = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // 左クリックが押されたら
-        if (IsMove)
-        {
-            // 向こう側へ -130 まで回転できるようにする（初めは 0 になっています）
-            limits.min = 90;
-        }
-        else
-        {
-            // デフォルトの位置で、左クリックが押されていないときは回転しない
-            limits.min = 0;
-        }
-        // デフォルトの位置ではドアが反発しない。
-        limits.bounciness = 0f;
-        hj.limits = limits;
-        
-    }*/
+    public bool reverse;
 
     void Start()
     {
@@ -51,7 +22,8 @@ public class DoorMove : MonoBehaviour
         if (RoTimer >= 0.01 && Counter <=45)
         {
             Debug.Log(Counter);
-            this.gameObject.transform.Rotate(0, Rotate, 0);
+            if (reverse) this.gameObject.transform.Rotate(0, Rotate, 0);
+            else this.gameObject.transform.Rotate(0, -Rotate, 0);
             Counter++;
             RoTimer = 0;
             
